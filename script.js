4f81a0067f7fcc0b8def44d42b77465ac28bfeee
ibrahim99elaw <ibrahimhanielaw@gmail.com>
2024-07-23 21:26:57 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+        // orders
+        let hearts = document.querySelectorAll('#heart');
+        hearts.forEach(heart => {
+            heart.addEventListener('click', function() {
+                if (heart.style.color === 'red') {
+                    heart.style.color = 'white';
+                } else {
+                    heart.style.color = 'red';
+                }
+            });
+        });
+
+            // requestes
+
+                document.querySelectorAll('.card-confirm-orders button').forEach(button => {
+            button.addEventListener('click', function() {
+                this.parentElement.remove();
+            });
+        });
+
+        
+
+
+
+          document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const menu = document.querySelector('.menu');
+
+    menuToggle.addEventListener('click', () => {
+      menu.classList.toggle('active');
+    });
+  });
